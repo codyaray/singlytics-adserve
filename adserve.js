7f6b54e257728447011b26820fefea8773c8a2ae
Cody A. Ray <sites@codyaray.com>
2012-09-29 15:55:05 -0500

    Adserve should use analytics data from hyperion and send to adnet

--- adserve.js
+++ adserve.js
@@ -4,6 +4,7 @@ var http = require('http'),
 
 var port = process.env.PORT || 3000;
 
+var hypeUrl = process.env.HYPE_URI;
 var adnetUrl = process.env.ADNET_URI;
 util.log("Using adnet URL: " + adnetUrl);
 
@@ -11,11 +12,33 @@ var app = express();
 
 app.get('/ad', function(request, response) {
   util.log(request.connection.remoteAddress + ": " + request.method + " " + request.url);
-  http.request(adnetUrl, function(adnet_response) {
-    response.writeHead(302, {'Location': adnet_response.headers["x-href"]});
-    response.end();
-  }).on('error', function(e) {
-    util.log('Problem with request to adnet: ' + e.message);
+  var query = require('url').parse(request.url, true).query || {},
+      appId = query.appId,
+      userId = query.account;
+  if (!appId || !userId) {
+    response.writeHead(400);
+    response.end('Missing required appId or account query-string params');
+    return;
+  }
+  var url = hypeUrl + appId + "/" + userId;
+  util.log(url);
+  http.request(url, function(hype_response) {
+    var body = "";
+    hype_response.on('data', function (chunk) {
+      body += chunk;
+    });
+    hype_response.on('end', function() {
+      util.log("Demographics: " + body);
+      var demographics = JSON.parse(body).demographics;
+      var url = adnetUrl + require('querystring').stringify(demographics);
+      util.log("Adnet URL: " + url);
+      http.request(url, function(adnet_response) {
+        response.writeHead(302, {'Location': adnet_response.headers["x-href"]});
+        response.end();
+      }).on('error', function(e) {
+        util.log('Problem with request to adnet: ' + e.message);
+      }).end();
+    });
   }).end();
 });
 
